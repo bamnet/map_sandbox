f78b177851db168253b2c538e23b4a29d0d54928
Brian Michalski <bmichalski@google.com>
2019-11-25 15:48:13 -0500

    Add a module to detect when the Maps API has loaded.
    And a helper function to easily detect when the API has loaded.

--- usgsts/src/loading_debug.ts
+++ usgsts/src/loading_debug.ts
@@ -1,3 +1,5 @@
+import {whenLoaded} from './onload';
+
 window.callback = () => {
   console.debug('Callback fired');
 };
@@ -6,24 +8,4 @@ window.onScriptLoad = () {
   console.debug('onLoad fired');
 }
 
-window.google = new Proxy(window.google || {}, {
-  set: (obj, prop, val) => {
-    obj[prop] = val;
-    console.debug('google set', prop);
-    setTimeout(() => {
-      console.debug('google set async', prop);
-    });
-    return true;
-  }
-});
-
-window.google.maps = new Proxy(window.google.maps || {}, {
-  set: (obj, prop, val) => {
-    obj[prop] = val;
-    console.debug('maps set', prop);
-    setTimeout(() => {
-      console.debug('maps set async', prop);
-    });
-    return true;
-  }
-});
+whenLoaded(() => console.log('Loaded!!!'));
--- usgsts/src/onload.ts
+++ usgsts/src/onload.ts
@@ -1,16 +1,80 @@
-window.google = window.google || {};
-window.google.maps = window.google.maps || {};
+export class LoadDetector {
+  private static instance: LoadDetector;
 
-Object.defineProperties(window.google.maps, {
-  'version': {
-    set(val) {
-      this._version = val;
-      setTimeout(() => {
-        console.log('google maps api loaded: ', google.maps.version);
-      });
-    },
-    get() {
-      return this._version;
+  private callbacks: Array<() => void> = [];
+
+  private constructor() {
+    window.google = window.google || {};
+    window.google.maps = window.google.maps || {};
+
+    if (this.alreadyLoaded()) {
+      return;
+    }
+
+    if (window['Proxy']) {
+      this.installListener();
+    } else {
+      this.installFallbackListener();
     }
   }
-});
+
+  static getInstance() {
+    if (!LoadDetector.instance) {
+      LoadDetector.instance = new LoadDetector();
+    }
+    return LoadDetector.instance;
+  }
+
+  onLoad(callback: () => void) {
+    if (this.alreadyLoaded()) {
+      callback();
+      return;
+    }
+    this.callbacks.push(callback);
+  }
+
+  runCallbacks() {
+    this.callbacks.forEach(c => c());
+  }
+
+  private installListener() {
+    console.debug('Installed Proxy listener in google.maps.version');
+    window.google.maps = new Proxy(window.google.maps, {
+      set: (obj, prop, val) => {
+        obj[prop] = val;
+        if (prop === 'version') {
+          setTimeout(() => {
+            LoadDetector.getInstance().runCallbacks();
+          });
+        }
+        return true;
+      }
+    });
+  }
+
+  private installFallbackListener() {
+    console.debug('Installed set-based listener in google.maps.version');
+
+    Object.defineProperties(window.google.maps, {
+      'version': {
+        set(val) {
+          this._version = val;
+          setTimeout(() => {
+            LoadDetector.getInstance().runCallbacks();
+          });
+        },
+        get() {
+          return this._version;
+        },
+      }
+    });
+  }
+
+  private alreadyLoaded() {
+    return Boolean('version' in google.maps && google.maps.version);
+  }
+}
+
+export function whenLoaded(callback: () => void) {
+  LoadDetector.getInstance().onLoad(callback);
+}
